c0f4b44df95397b7da60f6cd6b68bde6166e3188
swordstars <2986611877@qq.com>
2026-08-29 21:34:04 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -53,26 +53,27 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/my-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "四时小路komichi",
+	bio: "虚拟艺人团体VirtuaReal成员，以JK身份活动的都市传说
+四时小路参上！",
 	links: [
 		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+			name: "主页",
+			icon: "fa6-solid:link", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
+			url: "https://b23.tv/OOLOSTI",
 		},
 		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
+			name: "直播",
+			icon: "fa6-solid:link",
+			url: "https://b23.tv/hRDz042",
 		},
 		{
-			name: "GitHub",
-			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			name: "首播",
+			icon: "fa6-solid:link",
+			url: "https://b23.tv/4FAyrDg",
 		},
 	],
 };
